0e5d059e6e75bfb5f6962ae549a517101526011a
Jeevan Acharya <jeevan322379@gmail.com>
2019-10-03 23:35:01 -0500

    Project Initialize

---
+++ src/main/java/org/mumyoutube/MainController.java
@@ -0,0 +1,13 @@
+package org.mumyoutube;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String getString() {
+        return "Hello World!";
+    }
+}
